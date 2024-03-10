0b825ac3dd2ca2bf73f80ab9f5a4628931be665a
ri_yarm <thogusus@gmail.com>
2024-03-10 19:54:58 +0500

    fix: base url, after deploy gh-pages

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,4 +1,4 @@
 export const PASSWORD = "Valantis";
-export const BASE_URL = "http://api.valantis.store:40000/";
+export const BASE_URL = "https://api.valantis.store:41000/";
 export const BASE_INTERVAL = 500;
 export const ADS_PER_PAGE = 50;
